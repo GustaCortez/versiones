5e411e4210da10c2b95f149f99dcb5f57f04cdad
GustaCortez <melvincortey@gmail.com>
2024-08-02 21:24:37 -0600

    "Versión 5: Saludo en varios idiomas"

--- src/main/java/org/prueba/Main.java
+++ src/main/java/org/prueba/Main.java
@@ -7,9 +7,23 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Introduce tu nombre: ");
         String nombre = scanner.nextLine();
-        saludar(nombre);
+
+        System.out.print("Elige un idioma (1. Español, 2. Inglés): ");
+        int opcion = scanner.nextInt();
+
+        saludar(nombre, opcion);
     }
 
-    public static void saludar(String nombre) {
-        System.out.println("Hola, " + nombre + "!");
-    }}
+    public static void saludar(String nombre, int opcion) {
+        switch (opcion) {
+            case 1:
+                System.out.println("Hola, " + nombre + "!");
+                break;
+            case 2:
+                System.out.println("Hello, " + nombre + "!");
+                break;
+            default:
+                System.out.println("Idioma no soportado");
+        }
+    }
+}
