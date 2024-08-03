d8fb05ae0fc680a1c82717d51a017a354f766c9d
GustaCortez <melvincortey@gmail.com>
2024-08-02 21:21:40 -0600

    "Versión 4: Entrada del usuario"

--- src/main/java/org/prueba/Main.java
+++ src/main/java/org/prueba/Main.java
@@ -1,10 +1,15 @@
 package org.prueba;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String nombre = "Usuario";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce tu nombre: ");
+        String nombre = scanner.nextLine();
         saludar(nombre);
     }
 
     public static void saludar(String nombre) {
-        System.out.println("Hola, " + nombre + "!");}}
+        System.out.println("Hola, " + nombre + "!");
+    }}
